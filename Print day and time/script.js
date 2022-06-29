4f25df10d197d5b4aa8cb411706b46867de15d46
raartist <rishabhzee98@gmail.com>
2022-06-29 13:34:26 +0530

    first code

--- Print day and time/script.js
+++ Print day and time/script.js
@@ -1,3 +1,8 @@
+//1. Write a JavaScript program to display the current day and time in the following format.
+//Sample Output :
+//Today is : Tuesday.
+//Current time is : 10 PM : 30 : 38
+
 var date = new Date();
 var dateStr = date.toLocaleDateString("en-US", { weekday: "long" });
 
@@ -21,7 +26,7 @@ function getCustomTimeString() {
 
   document.getElementById("time").innerText = time;
 
-  let t = setTimeout(function () {
+  setTimeout(function () {
     getCustomTimeString();
   }, 1000);
   return time;
